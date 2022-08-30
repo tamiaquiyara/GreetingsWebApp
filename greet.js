85b434c6884bc3babdcc775ac950a77d180521a1
tamiaquiyara <mia.mbuckingworth@gmail.com>
2022-08-30 15:13:24 +0200

    Created greeted page

--- greet.js
+++ greet.js
@@ -1,10 +1,12 @@
-function Greetings(list) {
+module.exports = function Greetings(list) {
 
-    var localStorageNames = list || []
+    var localStorageNames = {}
     let nameWithoutChar = /^[A-z]+$/.test();
     
 
     function greet(personName, language) {
+
+       // localStorageNames.push(personName);
 
         if (language === "eng") {
             return "Hello, " + personName
@@ -15,43 +17,79 @@ function Greetings(list) {
         }
     }
 
-    function errorMessage(name) {
-        if (localStorageNames.includes(name)) {
-            return true
-        }
-        return false
+    // function errorMessage(name) {
+    //     if (localStorageNames.includes(name)) {
+    //         return true
+    //     }
+    //     return false
 
         
-    }
+    // }
         
 
-    
 
-    function setNames(personName) {
-        if (errorMessage(personName) === false) {
-            localStorageNames.push(personName)
-            return true
+    function storingNames(name){
+        if (localStorageNames[name] == undefined){
+            localStorageNames[name] = 1
         }
         else {
-            return false;
+            localStorageNames[name]++
         }
-
     }
+    
+
+    // function setNames(personName) {
+    //     if (errorMessage(personName) === false) {
+    //         localStorageNames.push(personName)
+    //         return true
+    //     }
+    //     else {
+    //         return false;
+    //     }
+
+    // }
+
+
+    function error(name, language){
+        let errorMsg = ''
+
+        if (name == '' && language == null) {
+            errorMsg = "Enter name and select a language"
+        
+          } else if (name == '') {
+            errorMsg = "Enter you name"
+        
+          } else if (language == null) {
+            errorMsg = "Select a Language";
+          }
+          return errorMsg
+    }
+
+    function personsCount(name){
+        
+    return localStorageNames[name]       
+    }
+
+
 
     function getNames() {
         return localStorageNames
     }
 
     function nameCount() {
-        return localStorageNames.length
+       let nameProperty = Object.keys(localStorageNames)
+        return nameProperty.length
     }
 
     return {
         greet,
-        setNames,
+       // setNames,
         getNames,
         nameCount,
-        errorMessage,
-        nameWithoutChar
+       // errorMessage,
+        nameWithoutChar,
+        storingNames,
+        error,
+        personsCount
     }
 }
--- index.js
+++ index.js
@@ -1,47 +1,78 @@
 let express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const Greeting = require('./greet');
 
 const app = express();
 const greetings = Greeting();
 
-app.engine('handlebars', exphbs.engine(
-  {defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
 
-app.get("/", function(req, res){
-  res.render("index",{
+app.get("/", function (req, res) {
+  res.render("index", {
 
   });
 });
 
-app.post("/greetings", function(req, res){
-  res.render("index",{
+app.post("/greetings", function (req, res) {
+  //console.log(req.body)
+  let name = req.body.text;
+  let language = req.body.TheLanguage;
+  let msg
+  if (name && language){
 
+     msg = greetings.greet(name, language);
+    greetings.storingNames(name)
+  }
+  let greeterr = greetings.error(name,language)
+
+  // if (name == '' && language == '') {
+  //   errorMsg = "Enter name and select a language"
+
+  // } else if (name == '') {
+  //   errorMsg = "Enter you name"
+
+  // } else if (language == '') {
+  //   errorMsg = "Select a Language";
+  // }
+
+  let namesCounted = greetings.nameCount()
+  // console.log(namesCounted);
+  // console.log(msg);
+  // console.log(errorMsg);
+  res.render("index", {
+    message: msg,
+    counter: namesCounted,
+    error: greeterr,
   });
 });
 
-app.post("/greeted", function(req, res){
-
-  greetings.greet();
-  res.redirect('/')
+app.get("/greeted", function (req, res) {
+  console.log(greetings.getNames());
+    let greetedNames = greetings.getNames()
+    
+  res.render("greeted", {
+    greetedNames
+  });
 });
 
-app.get("/counter/<USER_NAME>", function(req, res){
-  res.redirect('/')
+app.get("/counter/:name", function (req, res) {
+  req.params
+  res.render('counter',{
+    
+  })
 });
 
 let PORT = process.env.PORT || 3011;
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log('App started on port', PORT);
 });
